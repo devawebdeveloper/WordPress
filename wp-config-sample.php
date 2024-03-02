cc37b6e5f7131bf14d3ecca16bc0d704d78efe82
Deva <deva.webdeveloper@gmail.com>
2024-03-02 07:32:17 +0530

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -83,7 +83,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+$_SERVER['HTTPS']='on';
 
 /* That's all, stop editing! Happy publishing. */
 
